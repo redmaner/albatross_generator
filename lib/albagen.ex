b8bd971f7ecaa97f8555ca4367a5323bbb3b5b71
redmaner <jakevdputten@protonmail.com>
2021-09-19 18:18:35 +0200

    mix new albagen --sup

---
+++ lib/albagen.ex
@@ -0,0 +1,18 @@
+defmodule Albagen do
+  @moduledoc """
+  Documentation for `Albagen`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Albagen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/albagen_test.exs
@@ -0,0 +1,8 @@
+defmodule AlbagenTest do
+  use ExUnit.Case
+  doctest Albagen
+
+  test "greets the world" do
+    assert Albagen.hello() == :world
+  end
+end
